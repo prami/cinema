d7d798346456782dd31561c1de4e42d42121aad3
Michal Prawda <maykel99@gmail.com>
2010-05-10 00:31:25 +0200

    walidacja

--- app/models/showing.rb
+++ app/models/showing.rb
@@ -1,9 +1,11 @@
 class Showing < ActiveRecord::Base
   scope :today, where("date = ?", Date.today)
   scope :active, where("date >= ?", Date.today)
+  
   belongs_to :film
   belongs_to :room
-
+  validates_presence_of :film_id, :room_id, :date, :time
+  
   def self.group_by_films
     grouped = Showing.active.group_by{|i| i.film_id}
   end
@@ -11,4 +13,5 @@ class Showing < ActiveRecord::Base
   def self.week
     Showing.where("date >= ? and date <= ?", Date.today,  Date.today + 7.days)
   end
+
 end
